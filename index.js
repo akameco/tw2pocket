8583cdcb75ee008d27ff3f3aeb50c79925dbc46c
akameco <akameco.t@gmail.com>
2016-01-11 01:36:05 +0900

    Twitter startingを取得

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 'use strict';
+const Twitter = require('twit');
+
+const config = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+};
+
+const T = new Twitter(config);
+const userStream = T.stream('user');
+userStream.on('tweet', tweet => {
+  console.log(tweet.text);
+});
 
