6b34f0e540aa800ea354c26c16de090bff3a6d21
akameco <akameco.t@gmail.com>
2016-01-11 02:54:13 +0900

    Userフィルタリング

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
-const Twitter = require('twit');
-const save    = require('./pocket');
+const Twitter  = require('twit');
+const includes = require('array-includes');
+const save     = require('./pocket');
+const userList = require('./user');
 
 const config = {
   consumer_key: process.env.CONSUMER_KEY,
@@ -13,6 +15,8 @@ const T = new Twitter(config);
 const userStream = T.stream('user');
 
 userStream.on('tweet', tweet => {
+  if(!includes(userList, tweet.user.screen_name))
+    return;
   const len = tweet.entities.urls.length;
   if(len > 0){
     const url = tweet.entities.urls[len - 1].url;
